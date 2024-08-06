28ba374d2fda226025f00cdaad11d89b37de8a4a
Jon <jon.egeland@discordapp.com>
2024-08-05 23:02:27 -0700

    add wasm target for package triples

--- tools/src/napi.js
+++ tools/src/napi.js
@@ -15,6 +15,7 @@ export const NAPI_TARGET_MAP = {
   'win32-arm64-msvc': 'aarch64-pc-windows-msvc',
   'win32-ia32-msvc': 'i686-pc-windows-msvc',
   'win32-x64-msvc': 'x86_64-pc-windows-msvc',
+  wasm: 'wasm32-unknown-unknown',
 };
 
 /**
