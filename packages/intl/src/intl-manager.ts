74b7a6bb230cbe25fea528a73b39780a0c1e670b
Jon <jon.egeland@discordapp.com>
2024-10-12 14:02:15 -0700

    [runtime] Allow multi-segmented literal strings in `intl.string()`
    The current AST serialization method preserves special characters as their own entries in the result, such as `&`. For example, the message "Hello & Goodbye" would be serialized as:
    
    ```
    ["Hello ", "& ", "Goodbye"]
    ```
    
    The old method of `intl.string(t.SOME_MESSAGE)` only checked that the first element of the AST array was a string, and then only _rendered_ that element as well. This change makes it so that it checks all entries in the array to be strings and then renders them all joined together.
    
    Ideally the actual AST serialization would change here to save a bit more space, too, but this is close enough to start.

--- packages/intl/src/intl-manager.ts
+++ packages/intl/src/intl-manager.ts
@@ -120,20 +120,17 @@ export class IntlManager {
     const resolved = message(this.currentLocale);
     if (resolved == null || resolved.ast.length === 0) return '';
 
-    const value = resolved.ast[0] as LiteralNode;
-    if (typeof value !== 'string') {
-      throw new Error(
-        'Attempted to call `string` formatting on a non-literal message: ' +
-          JSON.stringify(resolved),
-      );
+    let result = '';
+    for (const element of resolved.ast) {
+      if (typeof element !== 'string') {
+        throw new Error(
+          'Attempted to call `string` formatting on a non-literal message: ' +
+            JSON.stringify(resolved),
+        );
+      }
+      result += element;
     }
-
-    // TODO: Figure out how to make this typing exact. This currently relies on
-    // the generic typing being sound enough to know that the message can only
-    // contain a single static text node and no placeholders or rich text.
-    // As it stands, this is _technically_ typed as string | undefined, even though
-    // TypeScript doesn't actually complain about it.
-    return value;
+    return result;
   }
 
   /**
