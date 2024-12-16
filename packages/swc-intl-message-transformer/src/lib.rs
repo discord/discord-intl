ab85c096a47f92b8db3f27595a99856f49c84566
Jon <jon.egeland@discordapp.com>
2024-12-16 09:20:22 -0800

    use `visit_mut_with` rather than `apply` for program

--- packages/swc-intl-message-transformer/src/lib.rs
+++ packages/swc-intl-message-transformer/src/lib.rs
@@ -1,5 +1,5 @@
 use swc_core::ecma::ast::Program;
-use swc_core::ecma::visit::visit_mut_pass;
+use swc_core::ecma::visit::VisitMutWith;
 use swc_core::plugin::{plugin_transform, proxies::TransformPluginProgramMetadata};
 
 use config::IntlMessageTransformerConfig;
@@ -8,15 +8,14 @@ mod config;
 mod transformer;
 
 #[plugin_transform]
-pub fn process_transform(program: Program, metadata: TransformPluginProgramMetadata) -> Program {
+pub fn process_transform(mut program: Program, metadata: TransformPluginProgramMetadata) -> Program {
     let config = serde_json::from_str::<IntlMessageTransformerConfig>(
         &metadata
             .get_transform_plugin_config()
             .expect("failed to get swc-intl-message-transformer plugin config"),
     )
-    .expect("failed to parse swc-intl-message-transformer config");
+        .expect("failed to parse swc-intl-message-transformer config");
 
-    program.apply(&mut visit_mut_pass(
-        transformer::IntlMessageConsumerTransformer::new(config),
-    ))
+    program.visit_mut_with(&mut transformer::IntlMessageConsumerTransformer::new(config));
+    program
 }
