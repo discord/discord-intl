a3d19bc9efa36f975648c04f785ddec01374ff00
Jon <jon.egeland@discordapp.com>
2025-03-17 12:12:51 -0700

    [json] Merge `Position` into `Message` to save allocations
    NAPI's object creation is incredibly slow compared to Node's internal creation, so we want to avoid it as much as possible. One simple (albeit slightly inconsistent) way to do that is to move the properties from a separate `Position` object into the `Message` object itself. That way Node just has to make one object and iterate its properties.
    
    See https://github.com/nodejs/node/issues/45905 for future updates about possibly making this faster.

--- crates/intl_flat_json_parser/src/napi.rs
+++ crates/intl_flat_json_parser/src/napi.rs
@@ -1,24 +1,23 @@
-use crate::{JsonMessage, JsonPosition};
+use crate::JsonMessage;
 use napi::bindgen_prelude::Array;
-use napi::{Env, Property};
+use napi::Env;
 use napi_derive::napi;
 
 // Use the mimalloc allocator explicitly when building the node addon.
 extern crate intl_allocator;
 
-#[napi(object)]
-pub struct Position {
+#[napi]
+pub struct Message {
+    pub key: String,
+    pub value: String,
+    // The Position properties are created directly inline on the message
+    // because NAPI object construction is pretty slow (like 10x slower than
+    // the engine creating an object directly), so moving these to direct
+    // properties of the class makes the whole process ~30-35% faster.
+    //
+    // See https://github.com/nodejs/node/issues/45905 for future updates.
     pub line: u32,
     pub col: u32,
-}
-
-impl From<JsonPosition> for Position {
-    fn from(pos: JsonPosition) -> Self {
-        Self {
-            line: pos.line,
-            col: pos.col,
-        }
-    }
 }
 
 fn collect_messages(env: Env, iterator: impl Iterator<Item = JsonMessage>) -> napi::Result<Array> {
@@ -27,47 +26,16 @@ fn collect_messages(env: Env, iterator: impl Iterator<Item = JsonMessage>) -> na
     // avoiding repeated re-allocations that we're pretty confident will happen
     // ends up saving a lot more time in the end.
     let mut result = env.create_array(1024)?;
-    // NAPI does not have an API for creating multiple instances of the same
-    // object structure, but we can get as close as possible by pre-defining
-    // object properties and cloning them to avoid allocating extra space for
-    // the same key many times over.
-    // See https://github.com/nodejs/node/issues/45905 for future updates.
-    let line_prop = Property::new("line")?;
-    let col_prop = Property::new("col")?;
-    let key_prop = Property::new("key")?;
-    let value_prop = Property::new("value")?;
-    let position_prop = Property::new("position")?;
     for message in iterator {
-        let key = env.create_string(&message.key)?;
-        let value = env.create_string(&message.value)?;
-        let mut position = env.create_object()?;
-        position.define_properties(&[
-            line_prop
-                .clone()
-                .with_value(&env.create_uint32(message.position.line)?),
-            col_prop
-                .clone()
-                .with_value(&env.create_uint32(message.position.col)?),
-        ])?;
-
-        let mut converted = env.create_object()?;
-        converted.define_properties(&[
-            key_prop.clone().with_value(&key),
-            value_prop.clone().with_value(&value),
-            position_prop.clone().with_value(&position),
-        ])?;
-
-        result.insert(converted)?;
+        result.insert(Message {
+            key: message.key.to_string(),
+            value: message.value.to_string(),
+            line: message.position.line,
+            col: message.position.col,
+        })?;
     }
 
     Ok(result)
-}
-
-#[napi(object)]
-pub struct Message {
-    pub key: String,
-    pub value: String,
-    pub position: Position,
 }
 
 #[napi(ts_return_type = "Message[]")]
