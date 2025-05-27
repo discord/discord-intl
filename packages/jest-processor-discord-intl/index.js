90af1fbf570ab9e0650cdb350d64edc70b789f5e
Jon <jon.egeland@discordapp.com>
2025-05-27 08:51:50 -0700

    [jest] Jest 29 compatibility Jest 17+ require preprocessors to return an object with a `code` property rather than just a plain string. Unsure if this even works on older versions, but it's required now, and Jest 29 is frankly already old at this point, so backwards compatibility is not a concern here.

--- packages/jest-processor-discord-intl/index.js
+++ packages/jest-processor-discord-intl/index.js
@@ -9,75 +9,83 @@ const {
   IntlCompiledMessageFormat,
 } = require('@discord/intl-loader-core');
 
+/**
+ * @param {string} source
+ * @param {string} filePath
+ * @param {object} _config
+ * @returns {string}
+ */
+function _processCode(source, filePath, _config) {
+  const precompileOptions = {
+    format: IntlCompiledMessageFormat.KeylessJson,
+    // Always bundle secrets when running tests
+    bundleSecrets: true,
+  };
+
+  const forceTranslation = filePath === '?forceTranslation';
+  if (isMessageDefinitionsFile(filePath) && !forceTranslation) {
+    const result = processDefinitionsFile(filePath, source, { processTranslations: false });
+    if (!result.succeeded) {
+      throw new Error(
+        `Failed to process intl messages file ${filePath}: ` + result.errors[0].message,
+      );
+    }
+
+    const sourceMessages = /** @type {Buffer} */ (
+      precompileFileForLocale(filePath, result.locale, undefined, precompileOptions)
+    ).toString();
+
+    result.translationsLocaleMap[result.locale] = 'intl$sentinel-use-local';
+
+    return new MessageDefinitionsTransformer({
+      messageKeys: result.messageKeys,
+      localeMap: result.translationsLocaleMap,
+      defaultLocale: result.locale,
+      getTranslationImport: (importPath) => {
+        // Embed source translations directly into the loader, because Jest doesn't have a loader
+        // mechanism to create new dependencies while transforming a file.
+        if (importPath === 'intl$sentinel-use-local') {
+          return `Promise.resolve({default: ${sourceMessages}})`;
+        }
+        return `Promise.resolve(require("${importPath}"))`;
+      },
+      debug: true,
+      // Jest won't pass the result of this transpilation through another processor, and expects
+      // babel-like transpilation to have occurred, so this processor exports transpiled-
+      // compatible values as a safe default.
+      exportMode: 'transpiledEsModule',
+    }).getOutput();
+  } else {
+    const locale = getLocaleFromTranslationsFileName(filePath);
+    if (isMessageTranslationsFile(filePath)) {
+      processTranslationsFile(filePath, source, { locale });
+    } else if (!forceTranslation) {
+      throw new Error(
+        'Expected a translation file or the `forceTranslation` query parameter on this import, but none was found',
+      );
+    }
+
+    const compiledResult = /** @type {Buffer} */ (
+      precompileFileForLocale(filePath, locale, undefined, precompileOptions)
+    );
+
+    return forceTranslation
+      ? 'module.exports.default = JSON.parse(' + JSON.stringify(compiledResult?.toString()) + ')'
+      : compiledResult.toString();
+  }
+}
+
 module.exports = {
   INTL_MESSAGES_FILE_PATTERN: '\\.messages\\.(js|json|jsona)(\\?forceTranslation)?$',
+
   /**
    * @param {string} source
    * @param {string} filePath
-   * @param {object} _config
-   * @returns {string}
+   * @param {object} config
+   * @returns {import('@jest/types').TransformTypes.TransformResult}
    */
-  process(source, filePath, _config) {
-    const precompileOptions = {
-      format: IntlCompiledMessageFormat.KeylessJson,
-      // Always bundle secrets when running tests
-      bundleSecrets: true,
-    };
-
-    const forceTranslation = filePath === '?forceTranslation';
-    if (isMessageDefinitionsFile(filePath) && !forceTranslation) {
-      const result = processDefinitionsFile(filePath, source, { processTranslations: false });
-      if (!result.succeeded) {
-        throw new Error(
-          `Failed to process intl messages file ${filePath}: ` + result.errors[0].message,
-        );
-      }
-
-      const sourceMessages = /** @type {Buffer} */ (
-        precompileFileForLocale(filePath, result.locale, undefined, precompileOptions)
-      ).toString();
-
-      result.translationsLocaleMap[result.locale] = 'intl$sentinel-use-local';
-
-      return new MessageDefinitionsTransformer({
-        messageKeys: result.messageKeys,
-        localeMap: result.translationsLocaleMap,
-        defaultLocale: result.locale,
-        getTranslationImport: (importPath) => {
-          // Embed source translations directly into the loader, because Jest doesn't have a loader
-          // mechanism to create new dependencies while transforming a file.
-          if (importPath === 'intl$sentinel-use-local') {
-            return `Promise.resolve({default: ${sourceMessages}})`;
-          }
-          return `Promise.resolve(require("${importPath}"))`;
-        },
-        debug: true,
-        // Jest won't pass the result of this transpilation through another processor, and expects
-        // babel-like transpilation to have occurred, so this processor exports transpiled-
-        // compatible values as a safe default.
-        exportMode: 'transpiledEsModule',
-      }).getOutput();
-    } else {
-      const locale = getLocaleFromTranslationsFileName(filePath);
-      if (isMessageTranslationsFile(filePath)) {
-        processTranslationsFile(filePath, source, { locale });
-      } else if (!forceTranslation) {
-        throw new Error(
-          'Expected a translation file or the `forceTranslation` query parameter on this import, but none was found',
-        );
-      }
-
-      const compiledResult = /** @type {Buffer} */ (
-        precompileFileForLocale(filePath, locale, undefined, precompileOptions)
-      );
-
-      if (forceTranslation) {
-        return (
-          'module.exports.default = JSON.parse(' + JSON.stringify(compiledResult?.toString()) + ')'
-        );
-      } else {
-        return compiledResult.toString();
-      }
-    }
+  process(source, filePath, config) {
+    const code = _processCode(source, filePath, config);
+    return { code, originalCode: source, sourceMapPath: null };
   },
 };
