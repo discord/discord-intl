8c0fc2f4c7bf331bd6e3dd2f0a3dc8d6539437be
Jon <jon.egeland@discordapp.com>
2025-06-02 09:01:32 -0700

    [json] Fix `get_locale_from_file_name` handling full paths
    I was pretty sure this couldn't happen from the requesting sides, where they should already be filtering the file path to just the base name, but it looks like at least one person has encountered it not filtering it properly, so let's just make the implementation more robust.

--- crates/intl_database_json_source/src/lib.rs
+++ crates/intl_database_json_source/src/lib.rs
@@ -3,12 +3,18 @@ use intl_database_core::{
     RawMessageTranslation,
 };
 use intl_flat_json_parser::parse_flat_translation_json;
+use std::path::Path;
 
 pub struct JsonMessageSource;
 
 impl MessageTranslationSource for JsonMessageSource {
     fn get_locale_from_file_name(&self, file_name: &str) -> KeySymbol {
-        file_name.split('.').next().unwrap_or("en-US").into()
+        Path::new(file_name)
+            .file_name()
+            .and_then(|p| p.to_str())
+            .map(|p| p.split_once(".").map_or(p, |(name, _ext)| name))
+            .unwrap_or("en-US")
+            .into()
     }
 
     fn extract_translations(
@@ -25,4 +31,29 @@ impl MessageTranslationSource for JsonMessageSource {
             )
         }))
     }
+}
+
+#[test]
+fn test_locale_from_file_name() {
+    assert_eq!(
+        key_symbol("en-US"),
+        JsonMessageSource.get_locale_from_file_name("foo/bar/baz/en-US.messages.jsona")
+    );
+    assert_eq!(
+        key_symbol("fr-FR"),
+        JsonMessageSource.get_locale_from_file_name("foo/bar/baz/fr-FR.messages.jsona")
+    );
+    assert_eq!(
+        key_symbol("notareal__locale#1"),
+        JsonMessageSource.get_locale_from_file_name("notareal__locale#1.messages.jsona")
+    );
+
+    assert_eq!(
+        key_symbol("da"),
+        JsonMessageSource.get_locale_from_file_name("da.messages.jsona")
+    );
+    assert_eq!(
+        key_symbol("cz"),
+        JsonMessageSource.get_locale_from_file_name("foo/bar/cz")
+    );
 }
